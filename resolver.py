647567a7e7aa3b29c8e49ddf942d393efe834048
Luca di Summa <149318306+Liukpro@users.noreply.github.com>
2026-05-05 21:00:39 +0200

    Update resolver.py

--- resolver.py
+++ resolver.py
@@ -16,26 +16,27 @@ def resolve_inputs(raw):
         if val is None:
             return default
         return val
+
     return {
         # FCCNOGC
-        "ric_op_mon": req["ric_op_mon"],
-        "cost_op_mon": req["cost_op_mon"],
-        "ammort": req["ammort"],
-        "tax": opt["imp"],
+        "ric_op_mon":           req("ric_op_mon"),
+        "cost_op_mon":          req("cost_op_mon"),
+        "ammort":               req("ammort"),
+        "tax":                  opt("imp"),
 
-        #FCFR
-        "patrimonio_netto": req["pat_net"],
-        "debiti_finanz": req["deb_f"],
+        # FCFR
+        "patrimonio_netto":     req("pat_net"),
+        "debiti_finanz":        req("deb_f"),
         "quota_rimbors_capital": opt("rimb_cap", 0),
 
-        #CCNO PER VARIAZIONE
-        "ccno1": opt("ccno", 0),  # se non hai serie storica
-        "ccno2": opt("ccno", 0),
+        # CCNO
+        "ccno1":                opt("ccno", 0),
+        "ccno2":                opt("ccno", 0),
 
-        #FCRf
-        "dividendi": opt("div", 0)
-        "oneri_finanz": opt["of"],
+        # FCRf
+        "dividendi":            opt("div", 0),
+        "oneri_finanz":         opt("of"),
 
-        #EXTRA
-        "liquidity": req["liq"],
-    }# da completare I cash flow con FCGC FCID, FCU, FCE, VARIAZIONE LIQUIDITà
+        # EXTRA
+        "liquidity":            req("liq"),
+    }
